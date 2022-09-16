5a0917d3c20b5f5f1ea8009847f4a777c61eb8a0
Denys Borysov <111238634+Dionissius@users.noreply.github.com>
2022-09-17 00:41:58 +0300

    add Thankyoupage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 
 import Button from './components/ui/Button';
 import Form from './components/Form';
-
+import Thankyoupage from './components/Thankyoupage/Thankyoupage';
 
 function App() {
   return (
     <>
-      <Button>
-        ЗАПИСАТИСЯ
-      </Button>
-      <Form>
-
-      </Form>
-
+      <Thankyoupage></Thankyoupage>
     </>
   );
 }
