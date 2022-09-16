4fcc77da1d038ece23ce226bf2f90cbd1fa023f0
Cashisfsg <cashisfsg@gmail.com>
2022-09-16 21:52:41 +0300

    Courses and Gallery

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@
 // import Button from './components/ui/Button';
 // import Form from './components/Form';
 import About from './components/About';
+import Courses from './containers/Courses';
+import Gallery from './containers/Gallery';
 
 
 function App() {
   return (
     <>
-      {/* <Button>
-        ЗАПИСАТИСЯ
-      </Button> */}
-      {/* <Form>
-      </Form> */}
-      <About>
-
-      </About>
+      <Courses />
+      <Gallery />
     </>
   );
 }
---
+++ src/containers/Courses.jsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Container from '../components/ui/Container';
+
+import CourseCard from '../components/CourseCard';
+
+const Courses = () => {
+
+    const courses = [
+        {id: 1, name: 'front-end', date: new Date(), lessons: 12, places: 18},
+        {id: 2, name: 'it project manager', date: new Date(), lessons: 12, places: 18},
+        {id: 3, name: 'ux/ui web design', date: new Date(), lessons: 12, places: 18},
+        {id: 4, name: 'front-end advanced', date: new Date(), lessons: 12, places: 18},
+        {id: 5, name: 'front-end', date: new Date(), lessons: 12, places: 18},
+        {id: 6, name: 'it project manager', date: new Date(), lessons: 12, places: 18},
+        {id: 7, name: 'ux/ui web design', date: new Date(), lessons: 12, places: 18},
+        {id: 8, name: 'front-end advanced', date: new Date(), lessons: 12, places: 18},
+    ]
+
+    return (
+        <Wrapper>
+            <Container>
+                <CoursesTitle>Наші курси</CoursesTitle>
+                <CoursesHeader>
+                    <HeaderText><b>On-line</b> навчання</HeaderText>
+                    <ButtonsArea>
+                        <button>dasda</button>
+                        <button>dasda</button>
+                        <button>dasda</button>
+                    </ButtonsArea>
+                </CoursesHeader>    
+                <CardsContainer>
+                    {courses.map(course => (
+                        <CourseCard 
+                            key={course.id}
+                            name={course?.name}
+                            date={course?.date}
+                            lessons={course?.lessons}
+                            places={course?.places}
+                        />
+                    ))}
+                </CardsContainer>
+            </Container>
+        </Wrapper>
+    );
+}
+
+export default Courses;
+
+const Wrapper = styled.section`
+
+`;
+
+const CoursesTitle = styled.h3`
+    margin-bottom: 16px;
+
+    font-family: 'Rubik';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 64px;
+    line-height: 138%;
+
+    letter-spacing: 4px;
+    text-transform: uppercase;
+
+    color: #232F3C;  
+`;
+
+const CardsContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 30px;
+`;
+
+const CoursesHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    width: 100%;
+
+    margin-bottom: 36px;
+
+`;
+
+const HeaderText = styled.span`
+    font-family: 'Rubik';
+    font-weight: 500;
+    font-size: 26px;
+    line-height: 185%;
+
+    letter-spacing: 2px;
+    text-transform: uppercase;
+
+    color: #333333;
+
+    & > b {
+        font-weight: 500;
+        color: #4FCC97;
+    }
+`;
+
+const ButtonsArea = styled.div`
+    display: flex;
+    margin-right: 10px;
+    
+    :last-child {
+        margin-right: 0;
+    }
+`;
