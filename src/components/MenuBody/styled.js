2bd31c269165551b78e96bd4fe1ade3f0a083a26
Yuri Kozyk <y.kozyk@gmail.com>
2022-09-17 12:12:19 +0300

    fix menu

--- src/components/MenuBody/styled.js
+++ src/components/MenuBody/styled.js
@@ -4,7 +4,7 @@ const Nav = styled.nav`
   display: flex;
   height: 100%;
   flex-direction: column;
-  justify-content: space-between;
+  justify-content: center;
   padding-left: 32px;
   text-align: left;
   position: absolute;
@@ -14,8 +14,10 @@ const Nav = styled.nav`
   opacity: ${({ isMenuOpen }) => (isMenuOpen ? '100' : '0')};
   transition: all 0.25s ease;
   z-index: 10;
-
-  @media (max-width: 540px) {
+  @media (max-width: 991px) {
+    justify-content: space-between;
+  }
+  @media (max-width: 567px) {
     padding-left: 16px;
   }
 `
