f5e1331f6ed806a7194b1e5e1a8fa704a8e62260
Cashisfsg <cashisfsg@gmail.com>
2022-09-17 22:24:39 +0300

    contacts adaptive

--- src/containers/Contacts.jsx
+++ src/containers/Contacts.jsx
@@ -16,12 +16,23 @@ const Contacts = forwardRef((props, ref) => {
 
         <Container ref={ref}>
             <ContactsWrapper>
+
+                <FormContainer>
+                    <FormHeader>
+                        <FormHeaderText>Залишилися <b>питання?</b></FormHeaderText>
+                        <FormHeaderIcon src={Question} alt="" />
+                    </FormHeader>
+                    <FormText>Заповни форму і наш спеціаліст передзвонить тобі, щоб розповісти про деталі курсу і відповісти на питання, що тебе цікавлять.</FormText>
+                    <Form />
+                </FormContainer>
+
                 <ContactsContainer>
                     <ButtonsArea>
                         <Button
                             height='40px'
                             width='120px'
                             margin='0 10px 0 0'
+                            padding='0'
                             size='14px'
                             lheight='114%'
                         >
@@ -31,6 +42,7 @@ const Contacts = forwardRef((props, ref) => {
                             height='40px'
                             width='120px'
                             margin='0 10px 0 0'
+                            padding='0'
                             size='14px'
                             lheight='114%'
                             bgcolor='none'
@@ -60,14 +72,7 @@ const Contacts = forwardRef((props, ref) => {
 
                     <SocialLinks />
                 </ContactsContainer>
-                <FormContainer>
-                    <FormHeader>
-                        <FormHeaderText>Залишилися <b>питання?</b></FormHeaderText>
-                        <img src={Question} alt="" />
-                    </FormHeader>
-                    <FormText>Заповни форму і наш спеціаліст передзвонить тобі, щоб розповісти про деталі курсу і відповісти на питання, що тебе цікавлять.</FormText>
-                    <Form />
-                </FormContainer>
+
 
             </ContactsWrapper>
         </Container>
@@ -80,31 +85,50 @@ export default Contacts;
 
 const ContactsWrapper = styled.section`
     position: relative;
-    height: 495px;
+    /* height: 495px; */
     width: 100%;
-    padding: 45px;
+    /* padding: 45px; */
     margin-bottom: 70px;
     background-color: yellowgreen;
     border-radius: 20px;
+
+    @media (max-width: 1170px) {
+        height: auto;
+    }
 `;
 
 const ContactsContainer = styled.div`
     display: flex;
     flex-direction: column;
-    height: 100%;
+    height: 495px;
     width: 100%;
-    max-width: 450px;  
-    background-color: grey;
+    padding: 45px;
+    /* max-width: 450px;   */
+    background: #232F3C;
+    border-radius: 20px;
+
+    @media (max-width: 1170px) {
+        /* height: 296px; */
+        padding: 26px 20px;
+    }
 `;
 
 const ButtonsArea = styled.div`
     display: flex;
     margin-bottom: 46px;
+
+    @media (max-width: 1170px) {
+        justify-content: center;
+        margin-bottom: 36px;
+    }
 `;
 
 const ContactInformation = styled.address`
     /* height: 100%; */
     flex: 1 1 auto;
+    @media (max-width: 1170px) {
+        flex: 0 1 auto;
+    }
 `;
 const ContactList = styled.ul`
     
@@ -129,6 +153,11 @@ const ContactItem = styled.li`
     & > img {
         margin-right: 16px;
     }
+
+    @media (max-width: 768px) {
+        font-size: 16px;
+        margin-bottom: 10px;
+    }
 `;
 
 const FormContainer = styled.div`
@@ -141,7 +170,18 @@ const FormContainer = styled.div`
     padding: 26px 36px;
 
     background: #232F3C;
-    border-radius: 20px;  
+    border-radius: 20px;
+    
+    @media (max-width: 1170px) {
+        width: 100%;
+        position: static;
+        transform: none;
+        margin-bottom: 30px;
+    }
+
+    @media (max-width: 768px) {
+        padding: 25px 20px 30px;
+    }
 `;
 
 const FormHeader = styled.div`
@@ -152,10 +192,18 @@ const FormHeader = styled.div`
     margin-bottom: 16px;
 `;
 
+const FormHeaderIcon = styled.img`
+    
+    @media (max-width: 450px) {
+        width: 56px;
+        height: 56px;
+    }
+`
+
 const FormHeaderText = styled.h4`
     font-family: 'Rubik';
     font-weight: 300;
-    font-size: 44px;
+    font-size: calc(24px + 20 * (100vw / 1760));
     line-height: 132%;
 
     letter-spacing: 4px;
@@ -176,5 +224,7 @@ const FormText = styled.p`
     font-size: 16px;
     line-height: 150%;
 
-    color: #FFFFFF;  
+    color: #FFFFFF; 
+    overflow: hidden;
+    text-overflow: ellipsis;
 `;
