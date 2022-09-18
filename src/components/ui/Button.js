08189ed1ff5bd6278e9607545c5788913532ac74
Cashisfsg <cashisfsg@gmail.com>
2022-09-18 13:53:47 +0300

    adaptive footer plus margin

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -19,6 +19,16 @@ const Button = styled.button`
   line-height: ${({ lheight }) => lheight || '100%'};
   text-align: center;
   text-transform: uppercase;
+  transition: all .3s linear; 
+
+  :hover {
+    /* background-color: #F5F6F9;
+    border: none;
+    color: #FFFFFF; */
+    transform: scale(102%);
+    box-shadow: 0px 0px 5px 4px rgba(230, 28, 58, 0.2);
+  }
+
   @media (max-width: 767.98px) {
     display: ${({ hiddenSm }) => (hiddenSm ? 'none' : 'inline-block')};
   }
